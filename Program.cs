4b362b86306f326b87e03b6ce5f55c249a799c02
Emiel Van de Waeter <emVandeWaeter@fluxus.nu>
2026-05-23 14:45:43 +0200

    Opening gemaakt, gebruiker ziet nu de instructie en kan typen in de console.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Geef een woord of zin in");
+            Console.ReadLine();
         }
     }
 }
